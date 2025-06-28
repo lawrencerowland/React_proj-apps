022ff4cfdf0f7eef7a5f9377ab1989487da3f7e9
lawrence rowland <lawrence.g.rowland@me.com>
2025-06-28 23:16:05 +0100

    fix capabilities wiring diagram app

--- apps/capabilities-wiring-diag/src/index.jsx
+++ apps/capabilities-wiring-diag/src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../../../src/common/index.css';
-import MarketTimelineExplorer from './app.jsx';
+import AICapabilitiesDiagram from './app.jsx';
 import reportWebVitals from '../../../src/common/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MarketTimelineExplorer />
+    <AICapabilitiesDiagram />
   </React.StrictMode>
 );
 
