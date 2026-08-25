fcb93b54fecfb428b45867dc005b1a6b18ad7180
lawrence rowland <lawrence.g.rowland@me.com>
2026-08-25 19:53:24 +0100

    Improve project controls graph interactions

--- apps/project-controls-knowledge-graph/src/index.jsx
+++ apps/project-controls-knowledge-graph/src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../../../src/common/index.css';
 import App from './App';
 import reportWebVitals from '../../../src/common/reportWebVitals';
 
